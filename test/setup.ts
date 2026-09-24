17e4d645e838e8a47e6c4aedf8492fc517e8070a
lucasreali <lucasrealioficial@gmail.com>
2026-09-24 13:55:17 -0300

    Revert "fix(app): wait for the terminal font before measuring the grid"
    This reverts commit ebb703958dc48f31198f007b7c6b9563b73ab0cb.

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,3 @@
 import { GlobalRegistrator } from "@happy-dom/global-registrator";
 
 GlobalRegistrator.register();
-
-// happy-dom has no FontFaceSet: every font is already loaded.
-Object.defineProperty(document, "fonts", { value: { load: async () => [] } });
